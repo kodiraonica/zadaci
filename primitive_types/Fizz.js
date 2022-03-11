082f4d2de35f109a0369cf4eb59b9554846c26f9
Vodeny <79132505+Vodeny@users.noreply.github.com>
2022-03-11 10:59:22 +0100

    Update Fizz.js

--- primitive_types/Fizz.js
+++ primitive_types/Fizz.js
@@ -1,7 +1,11 @@
-const broj = 64;
-if (broj % 3 == 0 ) {
+const broj = 60;
+if (broj % 15 == 0 ) {
+  console.log("fizzbuzz");
+} else if (broj % 3 == 0) {
   console.log("fizz");
-} else if (broj % 5 == 0) {
+} 
+else if (broj % 5 == 0) {
   console.log("buzz");
-} else {
+} 
+else {
   console.log(broj); }
